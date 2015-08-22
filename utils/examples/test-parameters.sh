5c05a63b3fd9634d6f506b3ca56fb21f46c48c7f
Ángel del Blanco Aguado <angel.dba84@gmail.com>
2015-08-22 17:21:30 +0200

    add example about using parameters in shell script

---
+++ utils/examples/test-parameters.sh
@@ -0,0 +1,9 @@
+#!/bin/ksh
+
+# extracted from http://osr600doc.sco.com/en/SHL_automate/_Passing_to_shell_script.html
+
+echo There are $# arguments to $0: $*
+echo first argument: $1
+echo second argument: $2
+echo third argument: $3
+echo here they are again: $@
