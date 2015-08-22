14c26ba81829bcdf5b487b4241233111f5a6fc71
Ángel del Blanco Aguado <angel.dba84@gmail.com>
2015-08-22 20:08:16 +0200

    Useful commands with stringa

---
+++ utils/strings/string.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+
+echo You have introduced $# arguments to script $0: $*
+echo first argument: $1
+echo second argument: $2
+echo contcat arguments: $1/$2
+echo here they are again: $@
